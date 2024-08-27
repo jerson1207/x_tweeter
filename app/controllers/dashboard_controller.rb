7f6446abd59a1a3507515cb0c223d60ffb84dd44
jersonjabat <jersonjabatstudent@gmail.com>
2024-08-27 21:30:26 +0800

    adjust view of tweets, improve layout design

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @tweets = Tweet.includes(:user, :liked_users, :bookmarked_users).order(created_at: :desc).map do |tweet|
       TweetPresenter.new(tweet: tweet, current_user: current_user) 
