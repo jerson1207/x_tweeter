8575b0f48c9072e38124e98c78942273ee01a911
jersonjabat <jersonjabatstudent@gmail.com>
2024-09-12 15:17:41 +0800

    Add counter_cache for follow status count

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,38 +2,27 @@ class FollowingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user
-    following = user.followings.create(following_params)
+    @user = User.find(params[:following_user_id])
+    current_user.followings.create(following_params)
     respond_to do |format|
-      format.html { redirect_to user_path(following.following_user) }
+      format.html { redirect_to user_path(@user) }
       format.turbo_stream
     end
-
-
   end
 
   def destroy
-    user
+    @user = User.find(params[:user_id])
     following = Following.find(params[:id])
     following.destroy
     respond_to do |format|
-      format.html { redirect_to user_path(following.following_user) }
+      format.html { redirect_to user_path(@user) }
       format.turbo_stream
     end
-    
   end
 
   private
 
-  def user
-    @user ||= User.find(params[:user_id])
-  end
-
-  def follower
-    @follower ||= user.following_user.find(params[:id])
-  end
-
   def following_params
-    params.permit(:following_user_id, :user_id)
+    params.permit(:following_user_id)
   end
 end
--- spec/requests/following_spec.rb
+++ spec/requests/following_spec.rb
@@ -1,29 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Following", type: :request do
-  let(:user1) { create(:user) }
-  let(:user2) { create(:user) }
+RSpec.describe "Followings", type: :request do
+  let(:current_user) { create(:user) }
+  let(:other_user) { create(:user) }
 
-  before { sign_in user1 }
+  before do
+    sign_in current_user  # Devise helper to authenticate current_user
+  end
 
-  describe 'POST #create' do
-    it "create a new following" do
+  describe "POST /users/:user_id/followings" do
+    it "creates a new following" do
       expect do
-        post user_followings_path(user1), params: {
-          following_user_id: user2.id
+        post user_followings_path(current_user), params: {
+          following_user_id: other_user.id
         }
-      end.to change { Following.count }.by(1)
-      expect(response).to redirect_to user_path(user2)
+      end.to change(Following, :count).by(1)
+      
+      expect(response).to redirect_to(user_path(other_user))
     end
   end
 
-  describe 'Delete #destroy' do
-    it 'descroy the existing following' do
-      following = create(:following, user: user1, following_user: user2)
-      expect do
-        delete user_following_path(user1, following)
-      end.to change { Following.count }.by(-1)
-      expect(response).to redirect_to user_path(user2)
+  describe "DELETE /users/:user_id/followings/:id" do
+    let!(:following) { current_user.followings.create(following_user_id: other_user.id) }
+
+    it "destroys the following" do
+      expect {
+        delete user_following_path(current_user, following.id)
+      }.to change(Following, :count).by(-1)
+    
+      expect(response).to redirect_to(user_path(current_user))
     end
   end
 end
